77c7a1ea3e7336c48b9784e6e097d0bc4ad901f1
Wenish <jonas.voland@bluewin.ch>
2016-08-22 00:31:48 +0200

    small changes

--- client/js/classes/viewModel.js
+++ client/js/classes/viewModel.js
@@ -6,4 +6,16 @@ app.ViewModel = function() {
   vm.closeProfil = function() {
     $('.popup-profil').fadeOut()
   }
+  vm.openSingleplayer = function() {
+    console.log('openSingleplayer')
+  }
+  vm.closeSingleplayer = function() {
+    console.log('closeSingleplayer')
+  }
+  vm.openMultiplayer = function() {
+    console.log('openMultiplayer')
+  }
+  vm.closeMultiplayer = function() {
+    console.log('closeMultiplayer')
+  }
 };
