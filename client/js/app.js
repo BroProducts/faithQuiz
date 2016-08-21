4e08c7585ca553585009a8615a97999cbce31ff7
Wenish <jonas.voland@bluewin.ch>
2016-08-21 20:51:51 +0200

    added fastclickjs

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,12 @@
 var app = app || {}
 app.version = '0.0.1'
 
+$(function() {
+    FastClick.attach(document.body);
+});
+
 $(document).ready(function(){
   app.vm = new app.ViewModel()
-  app.vm.musicMuted(true)
   console.log(ko.toJS(app.vm))
   ko.applyBindings(app.vm);
 })
