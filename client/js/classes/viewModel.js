948614e55ce6976a6186c658b3956536c5c81d8b
Wenish <jonas.voland@bluewin.ch>
2016-08-21 22:03:13 +0200

    added profil popup

--- client/js/classes/viewModel.js
+++ client/js/classes/viewModel.js
@@ -1,4 +1,9 @@
 app.ViewModel = function() {
   var vm = this;
-  
+  vm.openProfil = function(){
+    $('.popup-profil').fadeIn()
+  }
+  vm.closeProfil = function(){
+    $('.popup-profil').fadeOut()
+  }
 };
