cfa6573038da955a700a0c6f3e7b4497175ecc91
Wenish <jonas.voland@bluewin.ch>
2016-08-21 15:28:43 +0200

    removed audio

--- client/js/classes/viewModel.js
+++ client/js/classes/viewModel.js
@@ -1,14 +1,4 @@
 app.ViewModel = function() {
   var vm = this;
-  vm.musicMuted = ko.observable(false);
-  vm.musicMuted.subscribe(function (state) {
-    if(state){
-      $('#audio-relax')[0].muted = true;
-    } else {
-      $('#audio-relax')[0].muted = false;
-    };
-  });
-  vm.toggleMusic = function () {
-    vm.musicMuted(!vm.musicMuted())
-  }
+  
 };
