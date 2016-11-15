7038a0131e923224f34d5cacd58f71013340a357
raphael <raphaelbezd12@gmail.com>
2016-11-15 09:11:37 +0000

    updated port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || 3000
 const server = app.listen(port);
 
 server.on('listening', () =>
