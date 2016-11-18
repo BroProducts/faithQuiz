88520229665aad3484ad2640a5c7fa1b5868bf96
raphael <raphaelbezd12@gmail.com>
2016-11-18 11:18:39 +0100

    new login logic

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+var socket = io();
+var app = feathers()
+  .configure(feathers.socketio(socket))
+  .configure(feathers.hooks())
+  .configure(feathers.authentication({ storage: window.localStorage }))
+
+function login() {
+    app.authenticate({
+    type: 'local',
+    'username': document.getElementById("username").value,
+    'password': document.getElementById("password").value
+    }).then(function(result){
+    console.log('Authenticated!', result);
+    window.location = "game.html" 
+    }).catch(function(error){
+    console.error('Error authenticating!', error);
+    });
+}
