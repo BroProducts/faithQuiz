f2c9ba8ca39c60f681bc25f454ce75dc516b087d
Wenish <jonas.voland@bluewin.ch>
2016-11-19 01:04:57 +0100

    changed leaderboard loading

--- public/js/classes/viewModelLeaderboard.js
+++ public/js/classes/viewModelLeaderboard.js
@@ -1,28 +1,28 @@
 app.ViewModelLeaderboard = function() {
-    var vm = this;
-    vm.leaderboardloaded = ko.observable(false);
-    vm.leaderboarddata = ko.observableArray();
+  var vm = this;
+  vm.leaderboardloaded = ko.observable(false);
+  vm.leaderboarddata = ko.observableArray();
 
-    vm.start = function() {
-        app.showPreloaderFast();
+  vm.start = function() {
+    vm.leaderboardloaded(false);
+    console.log("start")
+    leaderboardService.find({
+      query: {
+        $sort: {
+          score: -1
+        },
+        $limit: 10
+      }
+    }).then(function(page) {
+      console.log(page)
 
-        console.log("start")
-        leaderboardService.find({
-            query: {
-            $sort: { score: -1 },
-            $limit: 10
-            }
-        }).then(function (page) {
-            console.log(page)
-            app.hidePreloaderFast();
-            
-            for (var i = 0; i < page.data.length; i++) {
-                var leaderboard = new app.leaderboard(page.data[i]);
-                console.log(leaderboard)
-                vm.leaderboarddata().push(leaderboard);
-            }
-            vm.leaderboardloaded(true) 
-            app.hidePreloaderFast();
-        });
-    };
+      for (var i = 0; i < page.data.length; i++) {
+        var leaderboard = new app.leaderboard(page.data[i]);
+        console.log(leaderboard)
+        vm.leaderboarddata().push(leaderboard);
+      }
+      vm.leaderboardloaded(true);
+    });
+  };
+  vm.start();
 };
--- public/js/classes/viewModelMain.js
+++ public/js/classes/viewModelMain.js
@@ -1,6 +1,5 @@
 app.ViewModelMain = function() {
   var vm = this;
-  vm.leaderboard = new app.ViewModelLeaderboard()
   vm.activeUser = ko.observable(app.get('user').username)
   vm.closePopup = function() {
     $('.popup').fadeOut()
@@ -16,13 +15,13 @@ app.ViewModelMain = function() {
   }
   vm.openLeaderboard = function() {
     $('.popup-leaderboard').fadeIn()
-    vm.leaderboard.start()
   }
   vm.logout = function() {
     app.logout().then(function () {
       return window.location.href = '/index.html';
     });
   }
-  vm.singlePlayer = new app.ViewModelSinglePlayer()
-  vm.multiPlayer = new app.ViewModelMultiPlayer()
+  vm.singlePlayer = new app.ViewModelSinglePlayer();
+  vm.multiPlayer = new app.ViewModelMultiPlayer();
+  vm.leaderboard = new app.ViewModelLeaderboard();
 };
