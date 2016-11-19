e56a203b2d3031916a3c1b207fcecf30112087ba
Wenish <jonas.voland@bluewin.ch>
2016-11-19 03:49:56 +0100

    updated mp vm

--- public/js/classes/viewModelMultiPlayer.js
+++ public/js/classes/viewModelMultiPlayer.js
@@ -1,5 +1,16 @@
 app.ViewModelMultiPlayer = function() {
     var vm = this;
+    vm.gameRequests = ko.observableArray();
+    vm.yourGameRequests = ko.observableArray();
+    vm.ongoingGames = ko.observableArray();
+    vm.completedGames = ko.observableArray();
     vm.friendslist = ko.observableArray();
+
+
+
+    vm.startNewGame() = function(){
+      console.log('start new game');
+    };
+
     console.log('MultiPlayer ViewModel loaded');
 };
