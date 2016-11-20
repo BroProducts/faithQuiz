7de137d25b8eedf59a7c0d8ea7713286caf64133
Wenish <jonas.voland@bluewin.ch>
2016-11-20 16:29:34 +0100

    mp update

--- public/js/classes/viewModelMultiPlayer.js
+++ public/js/classes/viewModelMultiPlayer.js
@@ -1,23 +1,27 @@
 app.ViewModelMultiPlayer = function() {
-    var vm = this;
-    vm.gameRequests = ko.observableArray();
-    vm.yourGameRequests = ko.observableArray();
-    vm.ongoingGames = ko.observableArray();
-    vm.completedGames = ko.observableArray();
-    vm.friendslist = ko.observableArray();
+  var vm = this;
+  vm.gameRequests = ko.observableArray();
+  vm.yourGameRequests = ko.observableArray();
+  vm.ongoingGames = ko.observableArray();
+  vm.completedGames = ko.observableArray();
+  vm.friendslist = ko.observableArray();
 
-    var $menuPage = $('.multiplayer-page-menu');
-    var $newGamePage =$('.multiplayer-page-new-game')
+  var $menuPage = $('.multiplayer-page-menu');
+  var $newGamePage = $('.multiplayer-page-new-game')
 
-    vm.startNewGame = function(){
-      $menuPage.hide();
-      $newGamePage.show();
-      console.log('start new game');
-    };
+  vm.openNewGamePage = function() {
+    $menuPage.hide();
+    $newGamePage.show();
+  };
 
-    vm.showGameRequest = function(){
-      console.log('show game request');
-    };
+  vm.closeNewGamePage = function() {
+    $newGamePage.hide();
+    $menuPage.show();
+  }
 
-    console.log('MultiPlayer ViewModel loaded');
+  vm.showGameRequest = function() {
+    console.log('show game request');
+  };
+
+  console.log('MultiPlayer ViewModel loaded');
 };
