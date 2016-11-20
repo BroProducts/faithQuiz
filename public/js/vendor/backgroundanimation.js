4d21b50ebb9309f04f6792b3b88f2c73674296b5
Wenish <jonas.voland@bluewin.ch>
2016-11-20 18:01:16 +0100

    changed background effect

--- public/js/vendor/backgroundanimation.js
+++ public/js/vendor/backgroundanimation.js
@@ -117,13 +117,13 @@
             ctx.clearRect(0,0,width,height);
             for(var i in points) {
                 // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                if(Math.abs(getDistance(target, points[i])) < 2000) {
                     points[i].active = 0.3;
                     points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                } else if(Math.abs(getDistance(target, points[i])) < 10000) {
                     points[i].active = 0.1;
                     points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
                     points[i].active = 0.02;
                     points[i].circle.active = 0.1;
                 } else {
