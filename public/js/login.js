2f2c1bcdd8e6e13d4d4c9eae5dc8959f6a619911
Wenish <jonas.voland@bluewin.ch>
2016-11-21 23:18:33 +0100

    added preloader on login page

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,33 @@ var socket = io();
 var app = feathers()
   .configure(feathers.socketio(socket))
   .configure(feathers.hooks())
-  .configure(feathers.authentication({ storage: window.localStorage }))
+  .configure(feathers.authentication({
+    storage: window.localStorage
+  }))
+hidePreloader()
 
 function login() {
-    var username = document.getElementById("username").value
-    var lcusername = username.toLowerCase()
-    app.authenticate({
+  $('.preloader').show()
+  var username = document.getElementById("username").value
+  var lcusername = username.toLowerCase()
+  app.authenticate({
     type: 'local',
     'username': lcusername,
     'password': document.getElementById("password").value
-    }).then(function(result){
+  }).then(function(result) {
     console.log('Authenticated!', result);
     window.location = "game.html"
-    }).catch(function(error){
+  }).catch(function(error) {
     console.error('Error authenticating!', error);
+    $('.preloader').hide()
     alert('Username or Password wrong');
-    });
+  });
+}
+
+function hidePreloader() {
+  $('.preloader').hide()
+}
+
+function showPreloader() {
+  $('.preloader').show()
 }
