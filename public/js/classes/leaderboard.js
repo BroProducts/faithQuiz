5d99806f270b968d429464a5420279fa946a9f96
Wenish <jonas.voland@bluewin.ch>
2016-11-21 21:16:13 +0100

    update leaderboard

--- public/js/classes/leaderboard.js
+++ public/js/classes/leaderboard.js
@@ -1,6 +1,14 @@
 app.leaderboard = function(options) {
+  var self = this;
   var datefull = new Date(options.createdAt);
   this.username = options.username;
   this.date = datefull.getDay() + "/" + datefull.getMonth() + "/" + datefull.getFullYear();
   this.score = options.score;
+  this.currentUserScore = computed(function(){
+    if(self.username.toLocaleLowerCase() == app.vm.profil.currentUsername().toLocaleLowerCase()){
+      return true;
+    } else {
+      return false;
+    }
+  })
 };
